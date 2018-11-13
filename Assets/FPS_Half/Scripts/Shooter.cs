905c505cd3e125dce38e026f999a04b792ed7522
David Bohan <madvlad00@googlemail.com>
2018-11-12 21:43:36 -0500

    Add hit scan

--- Assets/FPS_Half/Scripts/Shooter.cs
+++ Assets/FPS_Half/Scripts/Shooter.cs
@@ -6,6 +6,8 @@ public class Shooter : MonoBehaviour {
     public AudioSource reportSoundSource;
     public AudioClip reportSound;
     public Animation gunAnimation;
+    public GameObject flash;
+    public GameObject bulletHole;
 
     private float reloadTime = 2f;
     private float reloadChange = 0.0f;
@@ -25,6 +27,22 @@ public class Shooter : MonoBehaviour {
             this.reloadChange = this.reloadTime;
             this.reportSoundSource.PlayOneShot(reportSound);
             this.gunAnimation.Play();
+            this.flash.SetActive(true);
+            Invoke("HideFlash", 0.05f);
+
+            RaycastHit hit;
+            
+            if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, Mathf.Infinity))
+            {
+                var hitRotation = Quaternion.FromToRotation(Vector3.right, hit.normal);
+                Vector3 hitPoint = new Vector3(hit.point.x, hit.point.y, hit.point.z) + (hit.normal * 0.1f);
+                GameObject.Instantiate(bulletHole, hitPoint, hitRotation);
+            }
         }
+    }
+
+    void HideFlash()
+    {
+        this.flash.SetActive(false);
     }
 }
