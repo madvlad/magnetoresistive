2e6a1fede6f2651e4f8afe47619d4120df402d56
David Bohan <madvlad00@googlemail.com>
2018-11-24 14:02:49 -0500

    Add main menu

---
+++ Assets/FPS_Half/Scripts/LoadScene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour {
+    public string sceneName;
+    public GameObject aboutWindow;
+
+    public void LoadTheScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void ShowAbout()
+    {
+        aboutWindow.SetActive(true);
+    }
+
+    public void HideAbout()
+    {
+        aboutWindow.SetActive(false);
+    }
+}
