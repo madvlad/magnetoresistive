b09a6bf3123dc83214dc0ada4f9f4bba3866728b
David Bohan <madvlad00@googlemail.com>
2018-11-24 00:38:17 -0500

    Completely finish Level 1

--- Assets/FPS_Half/Scripts/Objectives/Level1Objectives.cs
+++ Assets/FPS_Half/Scripts/Objectives/Level1Objectives.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Level1Objectives : MonoBehaviour {
@@ -29,5 +30,11 @@ public class Level1Objectives : MonoBehaviour {
     {
         ErrorText.SetActive(false);
         GoodText.SetActive(true);
+        Invoke("LoadScene", 5.0f);
+    }
+
+    void LoadScene()
+    {
+        SceneManager.LoadScene("Level2");
     }
 }
