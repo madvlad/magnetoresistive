a07970436d3b3e6a31936f31eb2d02a01d9edd9a
Wesley King <kingwe92@gmail.com>
2018-11-24 19:31:35 -0500

    Init level 3 Mobo and PSU

---
+++ Assets/FPS_Half/Scripts/BlinkingLight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkingLight : MonoBehaviour {
+
+    public float blinkTime = 0.5f;
+	// Use this for initialization
+	void Start () {
+        InvokeRepeating("Blink", 0, blinkTime);
+    }
+
+    void Blink() {
+        gameObject.SetActive(!gameObject.active);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/FPS_Half/Scripts/SpinningFan.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinningFan : MonoBehaviour {
+
+    public float rotation = 1.0f;
+	void Start () {
+		
+	}
+	
+	void Update () {
+        transform.Rotate(0, rotation * Time.deltaTime, 0);
+    }
+}
