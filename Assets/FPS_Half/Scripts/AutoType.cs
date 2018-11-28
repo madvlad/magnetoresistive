e529f327a2134fba57113c0dbfcd9ebaa93f6db3
David Bohan <madvlad00@googlemail.com>
2018-11-27 21:47:07 -0500

    Add cutscene logic

---
+++ Assets/FPS_Half/Scripts/AutoType.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AutoType : MonoBehaviour
+{
+
+    public float letterPause = 0.2f;
+    public AudioClip sound;
+    public AudioClip sound2;
+
+    string message;
+
+    // Use this for initialization
+    void Start()
+    {
+        message = GetComponent<Text>().text;
+        GetComponent<Text>().text = "";
+        StartCoroutine(TypeText());
+    }
+
+    IEnumerator TypeText()
+    {
+        foreach (char letter in message.ToCharArray())
+        { 
+            GetComponent<Text>().text += letter;
+            if (sound && sound2)
+            {
+                int random = Random.Range(0, 1);
+                if (random == 1)
+                {
+                    GetComponent<AudioSource>().PlayOneShot(sound);
+                }
+                else
+                {
+                    GetComponent<AudioSource>().PlayOneShot(sound2);
+                }
+
+            }
+            yield return 0;
+            yield return new WaitForSeconds(letterPause);
+        }
+    }
+}
---
+++ Assets/FPS_Half/Scripts/Slideshow.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Slideshow : MonoBehaviour {
+
+    public List<GameObject> slidesInOrder;
+    public int currentPage = 0;
+    public string nextScene;
+
+    public void NextPage()
+    {
+        CrossFadeAlphaFixed(slidesInOrder[currentPage].GetComponent<Image>(), 0.0f, 2.0f, false);
+        slidesInOrder[currentPage].GetComponent<Image>().CrossFadeColor(Color.black, 2.0f, false, false);
+        currentPage++;
+        if (currentPage == slidesInOrder.Count)
+        {
+            SceneManager.LoadScene(nextScene);
+        }
+        else
+        {
+            CrossFadeAlphaFixed(slidesInOrder[currentPage].GetComponent<Image>(), 255.0f, 2.0f, false);
+            slidesInOrder[currentPage].GetComponent<Image>().CrossFadeColor(Color.white, 2.0f, false, false);
+        }
+    }
+    
+    void CrossFadeAlphaFixed(Graphic img, float alpha, float duration, bool ignoreTimeScale)
+    {
+        //Make the alpha 1
+        Color fixedColor = img.color;
+        fixedColor.a = 1;
+        img.color = fixedColor;
+
+        //Set the 0 to zero then duration to 0
+        img.CrossFadeAlpha(0f, 0f, true);
+
+        //Finally perform CrossFadeAlpha
+        img.CrossFadeAlpha(alpha, duration, ignoreTimeScale);
+    }
+}
